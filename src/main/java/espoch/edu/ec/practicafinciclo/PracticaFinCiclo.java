b2fbb2f8091d3375af61dda0d3078ea57c758aad
OswaldoBifarini <jgbifarini@gmail.com>
2025-02-13 14:52:45 -0500

    Ultima actualizacio XD

--- src/main/java/espoch/edu/ec/practicafinciclo/PracticaFinCiclo.java
+++ src/main/java/espoch/edu/ec/practicafinciclo/PracticaFinCiclo.java
@@ -1,12 +1,16 @@
 package espoch.edu.ec.practicafinciclo;
 
+import espoch.edu.ec.practicafinciclo.Vista.Vista;
+
 /**
  *
  * @author and_j
  */
 public class PracticaFinCiclo {
 
-    public static void main(String[] args) {
-        System.out.println("HOOLA GRUPO");
+     public static void main(String[] args) {
+        Vista objVista = new Vista();
+        objVista.setVisible(true);
+        
     }
 }
